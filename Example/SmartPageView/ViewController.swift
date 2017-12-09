c823a91d6b173488c9c034f0a5328f185f543a1c
shock <shock@docs.kr>
2017-12-09 09:12:04 +0900

    ^^

--- Example/SmartPageView/ViewController.swift
+++ Example/SmartPageView/ViewController.swift
@@ -11,18 +11,18 @@ import SmartPageView
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var smartSegmentHeaderView: SmartSegmentHeaderView!
     @IBOutlet weak var smartPageView: SmartPageView!
-    
-    var pageInfo: [(title: String, controller: UIViewController)] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var pageInfo: [(title: String, controller: UIViewController)] = []
+        
         for (index, color) in [UIColor.red,UIColor.blue,UIColor.green,UIColor.yellow,UIColor.purple].enumerated() {
             let ctrl = UIViewController()
             ctrl.view.backgroundColor = color
-            pageInfo.append((title:"PageX \(index+1)", controller:ctrl))
+            
+            pageInfo.append((title:"Page \(index+1)", controller:ctrl))
         }
         
         self.smartPageView.setPageControllers(self, pageInfo: pageInfo)
